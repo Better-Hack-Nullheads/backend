5c6169ce4c6382a647d7888f82bfdab94a4a7133
Niyat Debesay <niyatdebesay26@gmail.com>
2025-10-05 03:29:29 +0300

    feat: enable CORS for the application to allow cross-origin requests

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule,{
     bodyParser:false
   });
+  app.enableCors({
+    origin:"*"
+  })
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
