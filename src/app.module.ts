dfd4c073783e320d054dd194336ffb2d5a8d2823
Eba Adisu <EbaAdisuKenea>
2025-10-05 13:27:56 +0300

    auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import {  ConfigService } from '@nestjs/config';
-import { auth } from './lib/auth.js';
-import { AuthModule } from '@thallesp/nestjs-better-auth';
 import { DocumentModule } from './document/document.module.js';
 
 @Module({
@@ -11,15 +8,13 @@ import { DocumentModule } from './document/document.module.js';
     ConfigModule.forRoot({
       isGlobal: true, // Makes ConfigService available everywhere
     }),
-    AuthModule.forRoot({ auth }),
     MongooseModule.forRootAsync({
-      imports:[ConfigModule],
+      imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
-        uri: config.get<string>('MONGODB_URL') || 'mongodb://localhost:27017/mydb',
-
-      })
-
+        uri:
+          config.get<string>('MONGODB_URL') || 'mongodb://localhost:27017/mydb',
+      }),
     }),
     DocumentModule,
   ],
